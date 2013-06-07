659ed2e5a906fee590e276c546a29c749ed8922e
Brett VanderHaar <brett.vanderhaar@gmail.com>
2013-06-06 20:58:36 -0400

    start with specific gravity

---
+++ BVTemperatureTextField.h
@@ -0,0 +1,13 @@
+//
+//  BVTemperatureTextField.h
+//  homebrewcalc
+//
+//  Created by Brett VanderHaar on 6/6/13.
+//  Copyright (c) 2013 Brett VanderHaar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BVTemperatureTextField : UITextField
+
+@end
