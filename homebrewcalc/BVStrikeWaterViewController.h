139b0b765934ed809fca2e84c7291bbc5bf9dfa7
Brett VanderHaar <brett.vanderhaar@gmail.com>
2013-06-21 20:42:50 -0400

    add strike water

---
+++ homebrewcalc/BVStrikeWaterViewController.h
@@ -0,0 +1,13 @@
+//
+//  BVStrikeWaterViewController.h
+//  homebrewcalc
+//
+//  Created by Brett VanderHaar on 6/21/13.
+//  Copyright (c) 2013 Brett VanderHaar. All rights reserved.
+//
+
+#import "GAITrackedViewController.h"
+
+@interface BVStrikeWaterViewController : GAITrackedViewController
+
+@end
